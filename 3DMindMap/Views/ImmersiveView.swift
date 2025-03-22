b72e0ab1df24afb629dad900be005ac4c8d5f5ce
saki0411 <chipdale411@gmail.com>
2025-03-22 19:32:01 +0900

    add-TextFieldView

--- 3DMindMap/Views/ImmersiveView.swift
+++ 3DMindMap/Views/ImmersiveView.swift
@@ -11,86 +11,78 @@ import RealityKitContent
 struct ImmersiveView: View {
     @State private var cubes: [Entity] = []
     @State var model = ImmersiveViewModel()
-    
     @State var cube = Entity()
     @State var isTextFieldHidden = false
+    @State var nextWindowID = NewWindowID(id: 1)
+    
+    @Environment(\.openWindow) private var openWindow
     
     var body: some View {
-        ZStack{
+        RealityView { content in
             
             // 背景追加
-            RealityView { content, attachments in
-                var material = UnlitMaterial()
-                guard let resource = try? TextureResource.load(named: "space") else {
-                    fatalError("Couldn't load texture resource.")
-                }
-                material.color = .init(texture: .init(resource))
-                
-                let entity = Entity()
-                entity.components.set(ModelComponent(mesh: .generateSphere(radius: 1000), materials: [material]))
-                
-                entity.scale *= SIMD3(repeating: -1)
-                content.add(entity)
-            }attachments: {
-                Attachment(id: "uniqueId") {
-                    VStack{
-                        CommonTextForm(val: $model.inputText, placeholder: "文字を入力", onSubmit: {
-                            print("onSubmit")
-                            
-                        }
-                        )
-                        .opacity(isTextFieldHidden ? 0 : 1)
-                    }
-                }
-                
+            var material = UnlitMaterial()
+            guard let resource = try? TextureResource.load(named: "space") else {
+                fatalError("Couldn't load texture resource.")
             }
-            .glassBackgroundEffect()
-            .tag("uniqueId")
+            material.color = .init(texture: .init(resource))
+            
+            let entity = Entity()
+            entity.components.set(ModelComponent(mesh: .generateSphere(radius: 1000), materials: [material]))
+            
+            entity.scale *= SIMD3(repeating: -1)
+            content.add(entity)
             
             
-            RealityView { content in
-                
-                
-                let scene = model.setupContentEntity()
-                content.add(scene)
-                
-                
-                for cube in cubes {
-                    scene.addChild(cube)
-                }
+            let scene = model.setupContentEntity()
+            content.add(scene)
+            
+            
+            for cube in cubes {
+                scene.addChild(cube)
             }
-            .gesture(
-                DragGesture()
-                    .targetedToAnyEntity()
-                    .onChanged { value in
-                        let entity = value.entity
-                        let newPos = value.convert(value.location3D, from: .local, to: entity.parent!)
-                        
-                        // 位置を変更
-                        entity.position = newPos
-                        
-                        // ノード情報も更新
-                        model.updateNodePosition(entity: entity, newPosition: newPos)
-                    }
-            )
-            .gesture(
-                TapGesture()
-                    .onEnded {
+            
+        }
+        .gesture(
+            DragGesture()
+                .targetedToAnyEntity()
+                .onChanged { value in
+                    let entity = value.entity
+                    let newPos = value.convert(value.location3D, from: .local, to: entity.parent!)
+                    
+                    // 位置を変更
+                    entity.position = newPos
+                    
+                    // ノード情報も更新
+                    model.updateNodePosition(entity: entity, newPosition: newPos)
+                }
+        )
+        .gesture(
+            TapGesture()
+                .onEnded {
+                    
+                    if isTextFieldHidden {
                         isTextFieldHidden.toggle()
                         model.addCube(text: "aaa", parentId: "aaa")
-                        
+                    }else{
+                        isTextFieldHidden.toggle()
                     }
-            )
-            //デバッグ用でとりあえず一個
-            .onAppear(){
-                model.addCube(text: "aaa", parentId: "aaa")
-                
-            }
+                }
+        )
+        //デバッグ用でとりあえず一個
+        .onAppear(){
+            model.addCube(text: "aaa", parentId: "aaa")
+            
+        }
+        .onChange(of: isTextFieldHidden){
+            openWindow(value: nextWindowID.id)
             
         }
         
     }
+    
 }
+
 
 
 #Preview(immersionStyle: .full) {
