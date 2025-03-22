daf9691f6a30b21190a538f1528df336618544d6
tiger121005 <itiger4649@gmail.com>
2025-03-23 07:26:02 +0900

    ちゃんとこっち向くようにした！

--- 3DMindMap/Utils/Manager/Mesh/CreateMesh.swift
+++ 3DMindMap/Utils/Manager/Mesh/CreateMesh.swift
@@ -19,20 +19,15 @@ class CreateMesh {
         
         nodeEntity.transform.translation = SIMD3(position)
         nodeEntity.name = id
-        
+        nodeEntity.components.set(BillboardComponent())
         // ドラッグ可能にするための必須コンポーネントを追加
         // 1. 衝突判定 (CollisionShapeComponent)
         let collisionShape = ShapeResource.generateBox(size: [0.3, 0.2, 0.1])
-        nodeEntity.components.set(CollisionComponent(shapes: [collisionShape],
-  isStatic: false,
-   filter: .default))
+        nodeEntity.components.set(CollisionComponent(shapes: [collisionShape], isStatic: false, filter: .default))
         
         // 2. 物理特性の設定 (PhysicsBodyComponent)
-        let physicsMaterial = PhysicsMaterialResource.generate(friction: 0.8, restitution: 0.0)
-        nodeEntity.components.set(PhysicsBodyComponent(shapes: nodeEntity.collision!.shapes,
-mass: 0.0,
- material: physicsMaterial,
-                                                       mode: .dynamic))
+//        let physicsMaterial = PhysicsMaterialResource.generate(friction: 0.8, restitution: 0.0)
+//        nodeEntity.components.set(PhysicsBodyComponent(shapes: nodeEntity.collision!.shapes, mass: 0.0, material: physicsMaterial, mode: .dynamic))
         // 3. ユーザーインタラクションのターゲット設定 (InputTargetComponent)
         nodeEntity.components.set(InputTargetComponent(allowedInputTypes: .indirect))
         
@@ -61,7 +56,6 @@ mass: 0.0,
         textEntity.orientation = simd_quatf(angle: 0, axis: [0, 1, 0])
         
         //  textEntity.transform.translation = SIMD3(position)
-        
         
         return textEntity
     }
