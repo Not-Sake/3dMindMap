f66155ea2a086300ecc8845f9f8d101b22fdffb3
saki0411 <chipdale411@gmail.com>
2025-03-23 13:04:48 +0900

    add-animation

--- 3DMindMap/Views/ImmersiveView.swift
+++ 3DMindMap/Views/ImmersiveView.swift
@@ -14,7 +14,7 @@ struct ImmersiveView: View {
     @State var nextWindowID = NewWindowID(id: 1)
     @Environment(\.openWindow) private var openWindow
     @ObservedObject var gestureModel: HeartGestureModel
-    
+    @State var load = false
     
     var body: some View {
         RealityView { content in
@@ -36,6 +36,29 @@ struct ImmersiveView: View {
             
             for cube in model.cubes {
                 model.animateOpacity(nodeEntity: cube)
+            }
+            if load == true{
+                // memo もっと後ろに配置する
+                do {
+                    // heartanimationを非同期で読み込む
+                    let heartModel = try await Entity(named: "heartanimation", in: RealityKitContent.realityKitContentBundle)
+                    heartModel.scale = SIMD3(repeating: 0.1)
+                    
+                    // Y軸を反転
+                    heartModel.scale *= SIMD3(x: 1, y: -1, z: 1)
+                    
+                    if let animation = heartModel.availableAnimations.first {
+                        heartModel.playAnimation(animation.repeat())
+                    }
+                    
+                    // シーンにheartModelを追加
+                    scene.addChild(heartModel)
+                    
+                } catch {
+                    // エラーハンドリング
+                    print("Error loading entity or playing animation: \(error)")
+                }
+                
             }
         }
         .task {
