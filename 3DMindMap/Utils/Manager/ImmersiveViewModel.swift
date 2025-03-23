cc8d005241cb7fd14d42db752efd355ac8cc3f68
HASURO <rentaro170928@icloud.com>
2025-03-23 10:35:47 +0900

    fix: func architect

--- 3DMindMap/Utils/Manager/ImmersiveViewModel.swift
+++ 3DMindMap/Utils/Manager/ImmersiveViewModel.swift
@@ -22,12 +22,47 @@ final class ImmersiveViewModel {
         return contentEntity
     }
     
-    func addNode(inputText: String, parentId: String, position: Point3D, id: String) {
-        nodes.append(NodeType(id: id, topic: inputText, parentId: parentId, position: position))
+    func findNode(id: String) -> NodeType? {
+        if let index = nodes.firstIndex(where: { "\($0.id)" == id }) {
+            return nodes[index]
+        }
+        return nil
     }
     
-    func addEntity(id: String, posision: Point3D, text: String) -> Entity {
-        let entity = CreateMesh().createNode(id: id, position: posision)
+    func findChild(parentId: String) -> NodeType? {
+        if let index = nodes.firstIndex(where: { "\($0.parentId)" == parentId }) {
+            return nodes[index]
+        }
+        return nil
+    }
+    
+    func addNodeData(inputText: String, parentId: String, position: Point3D, id: String) -> NodeType? {
+        let parentNode = findNode(id: parentId)
+        if parentNode == nil {
+            return nil
+        }
+        let childColor = findChild(parentId: parentId)?.bgColor
+        let color = parentNode?.childrenCount == 0 ? RandomColor().getRandomColor(parentNode?.bgColor) : childColor
+        let newNode = NodeType(id: id, topic: inputText, parentId: parentId, position: position, bgColor: color, frameColor: nil)
+        nodes.append(newNode)
+        
+        if parentNode?.childrenCount == 0 {
+            if let index = cubes.firstIndex(where: { "\($0.name)" == "\(parentId)" }) {
+                let entity = cubes[index]
+                if let parentBorder = entity.children.first(where: { "\($0.name)" == "border_\(parentId)" }) {
+                    let borderUIColor = UIColor(color ?? Color.black)
+                    let borderMaterial = UnlitMaterial(color: borderUIColor)
+                    parentBorder.components[ModelComponent.self]?.materials = [borderMaterial]
+                    parentNode?.frameColor = color
+                }
+            }
+        }
+        parentNode?.childrenCount += 1
+        return newNode
+    }
+    
+    func createNodeEntity(id: String, posision: Point3D, text: String, bgColor: Color?) -> Entity {
+        let entity = CreateMesh().createNode(id: id, position: posision, bgColor: bgColor ?? Color.white, borderColor: nil)
 
         entity.name = id
         entity.components.set(InputTargetComponent(allowedInputTypes: .indirect))
@@ -43,35 +78,33 @@ final class ImmersiveViewModel {
         return entity
     }
     
-    func addCube(text:String) {
-        print("called addCube")
+    func addNode(text:String) {
         let id = UUID().uuidString
-        print("selectedNodeId: \(selectedNodeId)")
         let position = CalculatorManager().newPosition(parentId: selectedNodeId, nodes: nodes)
-        print("new position: \(position)")
-        let newCube = addEntity(
+        let newEntity = addNodeData(inputText: inputText, parentId: selectedNodeId, position: position, id: id)
+        let newCube = createNodeEntity(
             id: id,
             posision: position,
-            text:text
+            text: text,
+            bgColor: newEntity?.bgColor
         )
         cubes.append(newCube)
-        addNode(inputText: inputText, parentId: selectedNodeId, position: position, id: id)
         inputText = ""
-        dump(nodes)
     }
     
-    func addInitialCube(text: String) {
+    func addInitialNode(text: String) {
         let x: Float = 0
-        let y: Float = 1.2
-        let z: Float = 1.2
+        let y: Float = 1
+        let z: Float = -1
         let id = UUID().uuidString
         
-        let newCube = addEntity(
+        let newCube = createNodeEntity(
             id: id,
-            posision: Point3D(x: x, y: y, z: z), text: text
+            posision: Point3D(x: x, y: y, z: z), text: text,
+            bgColor: Color.white
         )
         cubes.append(newCube)
-        addNode(inputText: inputText, parentId: "", position: Point3D(x: x, y: y, z: z), id: id)
+        nodes.append(NodeType(id: id, topic: inputText, parentId: "", position: Point3D(x: x, y: y, z: z), bgColor: Color.white, frameColor: nil))
         inputText = ""
     }
     
